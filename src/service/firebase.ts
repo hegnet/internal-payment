cdd0cf1331b9f041c5357069bc83973b6529a16a
Mikkel Anderson <mra@npstoday.com>
2022-08-29 10:59:43 +0200

    Plain text

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -9,14 +9,14 @@ import {Product} from "../models/Product";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
+    apiKey: "AIzaSyBD1lwryVJDoxfXQsimAcrGplB6dC8fjBo",
+    authDomain: "hegnets-bartendershop.firebaseapp.com",
+    projectId: "hegnets-bartendershop",
+    storageBucket: "hegnets-bartendershop.appspot.com",
+    messagingSenderId: "1079071601788",
+    appId: "1:1079071601788:web:3458ee10c77d27c58905f9",
+    measurementId: "G-B0LB362MJN",
+    databaseURL: "https://hegnets-bartendershop-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 // Initialize Firebase
